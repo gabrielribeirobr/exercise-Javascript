8c3c2bc39015a536ffa2e963dff77dce7943cba8
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-04-15 19:00:37 +0000

    callback functions

--- practice-functions/pracFunc.js
+++ practice-functions/pracFunc.js
@@ -1,7 +1,23 @@
-let sayHi = function(){
+/* let sayHi = function(){
     console.log("hello world");
 }
 
 let func = sayHi;
 
-func();
+func();  */
+
+function ask(question, yes, no){
+    if (confirm(question)) yes()
+
+    else no()
+}
+
+function showOk(){
+    alert("Voce concordou");
+}
+
+function showCancel(){
+    alert("Voce discordou")
+}
+
+ask("Você concorda que o Vasco é o melhor time do Brasil?", showOk, showCancel);
