55ea781972ef3a0395bc8695504792d416033640
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-04-21 18:19:31 +0000

    add commentary

--- practice-objects/objects.js
+++ practice-objects/objects.js
@@ -5,14 +5,14 @@ let user = {
 };
 
 let age;
-
+//  verifica se a idade é válida
 do{
     age = parseInt(prompt('Qual é a sua idade? '));
     if(isNaN (age) || age < 0 ){
         console.log('Insira uma idade válida.');
     }
 } while (isNaN(age) || age < 0);
-
+// quando a idade for válida vamos incrementar o objeto USER com "age"
 user.age = age;
 console.log(`Seu nome é ${user.name} e sua idade é ${user.age} anos`);
         
