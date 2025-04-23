46a6b91427180642847949bf91ed7b27ea9a915d
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-04-23 19:42:42 +0000

    fix syntax object

--- practice-objects/objects-practice.js
+++ practice-objects/objects-practice.js
@@ -6,20 +6,21 @@ const brands = {
     toyota: 'toyota',
     honda: 'honda'
 }
+const car = {
+    
+}
 
 function checkBrands() {
     const result = prompt('Digite a marcas de carro que possui (exemplo: nissan, toyota, honda): ')
     if( result in brands){
-        brands.car = result;
+        car.brand = result;
         console.log(car);
-        modelo();
+        // modelo(); ainda por criar
     } console.log('Digite uma das marcas como exemplo')
 }
 checkBrands()
 
-const car = {
-    
-}
+
 
 
 /* modelo = () => {
