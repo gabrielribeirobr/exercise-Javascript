231f5597e11bb22fcfcea4ee07e1c23329d50bb2
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-04-23 19:59:01 +0000

    fix loop function

--- practice-objects/objects-practice.js
+++ practice-objects/objects-practice.js
@@ -14,23 +14,23 @@ function checkBrands() {
     const result = prompt('Digite a marcas de carro que possui (exemplo: nissan, toyota, honda): ')
     if( result in brands){
         car.brand = result;
-        console.log(car);
-        // modelo(); ainda por criar
-    } console.log('Digite uma das marcas como exemplo')
+        carModel(); 
+    } else {console.log('Digite uma das marcas como exemplo')
+        checkBrands();
+        }
 }
 checkBrands()
 
+function carModel(){
+    const model = prompt('Qual o modelo do seu carro?: ')
+    car.model = model;
+    carAge();
+}
 
-
-
-/* modelo = () => {
-    if(carro.marca == 'nissan' || carro.marca == 'toyota' || carro.marca == 'honda' ){
-        modelo = prompt('Informe o modelo do carro: ')
-        carro.modelo = modelo;
-
-    }
+function carAge(){
+    const age = prompt('Qual o ano do seu carro? ')
+    car.age = age;
+    console.log(car);
 }
 
 
-
-modelo(); */
