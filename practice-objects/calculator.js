bb0f7f408db6492f00e9f5bc2afabb8fb477da81
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-04-24 19:37:06 +0000

    novo desafio utilizando funcao construtora

--- practice-objects/calculator.js
+++ practice-objects/calculator.js
@@ -1,6 +1,6 @@
 let prompt = require('prompt-sync')();
 
-function Calculator(){
+/* function Calculator(){
     this.read = function(){
         this.a = +prompt('a? ');
         this.b = +prompt('b? ');
@@ -19,5 +19,19 @@ let calculator = new Calculator();
 calculator.read();
 
 console.log('Soma=' + calculator.sum());
-console.log('Multiplicacao=' + calculator.mul());
+console.log('Multiplicacao=' + calculator.mul()); */
 
+
+function Pessoa(){
+    this.read = function(){
+        this.name = prompt('Digite seu nome ');
+        this.age = +prompt('Digite sua idade');
+    };
+
+    function read(){
+        return console.log(Pessoa);
+    };
+}
+
+let pessoa1 = pessoa();
+read.Pessoa();
