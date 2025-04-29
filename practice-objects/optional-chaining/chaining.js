e28fef39e591fb99848ca858218feb3a672aeed8
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-04-29 18:29:43 +0000

    solution to verify undefined properties

--- practice-objects/optional-chaining/chaining.js
+++ practice-objects/optional-chaining/chaining.js
@@ -6,6 +6,12 @@ console.log(user.adress.street); */ // aqui ele retornará um erro
 
 // para verificar uma propridade inesiste devemos utilizar o "if (?)"
 
-let user = {}
+/* let user = {}
 
-console.log(user.adress ? user.adress.street : undefined); // funciona porém repete muitas vezes a propriedade "user.adress"
+console.log(user.adress ? user.adress.street : undefined); */ // funciona porém repete muitas vezes a propriedade "user.adress"
+
+// para resolver esse problema podemos escrever assim o código
+
+let user = {};
+
+console.log( user?.adress?.street ); //assim verificamos passo a passo sem repetir as propriedades do objeto.
