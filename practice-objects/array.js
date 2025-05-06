74df2253f7abba3b6ce061a32699e39413531b17
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-05-06 09:29:43 +0000

    removendo elemento do array

--- practice-objects/array.js
+++ practice-objects/array.js
@@ -2,29 +2,33 @@ const prompt = require('prompt-sync')();
 
 let carros = ['toyota', 'honda', 'nissan', 'ford','bmw'];
 
+console.log(carros[0], carros[1],carros[2],carros[3], carros[4]);
+
+
+carros[3] = 'mazda';  // podemos substituí um elemento ao array
+
+/* console.log('================');
+console.log(carros[0], carros[1],carros[2],carros[3],  '(troca de elementos)', carros[4]); */ // agora substituí o elemento 3 por 'mazda' console.log(carros[4]);
+
+// ou modelos incrementar um novo elemento ao array
+
+carros[5] = 'mitsubishi';
+
+/* console.log('================');
 console.log(carros[0]);
 console.log(carros[1]);
 console.log(carros[2]);
 console.log(carros[3]);
 console.log(carros[4]);
-
-carros[3] = 'mazda';  // podemos substituí um elemento ao array
-
+console.log(carros[5], '(novo elemento ao array)'); // novo elemento
 console.log('================');
-console.log(carros[0]);
-console.log(carros[1]);
-console.log(carros[2]);
-console.log(carros[3], '(troca de elementos)'); // agora substituí o elemento 3 por 'mazda' 
-console.log(carros[4]);
+ */
+console.log('números de elementos no array = ',carros.length); //contagem de elementos ao array
 
-// ou modelos incrementar um novo elemento ao array 
+console.log(carros); // mostrando o array inteiro
 
-carros[5] = 'mitsubishi';
+console.log(carros.at(-1), '(ultimo elemento do array)'); // para obter o último elemtno do array
 
-console.log('================');
-console.log(carros[0]);
-console.log(carros[1]);
-console.log(carros[2]);
-console.log(carros[3]); 
-console.log(carros[4]);
-console.log(carros[5], '(novo elemento ao array)'); // novo elemento 
+console.log(carros.pop()); 
+console.log(carros); // .pop mostra o último valor do array e o remove
+
