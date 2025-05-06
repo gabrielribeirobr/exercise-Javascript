ff8806c756b16b5ed4bbd5784d4dad449abb466b
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-05-06 08:59:12 +0000

    practice array

---
+++ practice-objects/array.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+
+let carros = ['toyota', 'honda', 'nissan', 'ford','bmw'];
+
+console.log(carros[0]);
+console.log(carros[1]);
+console.log(carros[2]);
+console.log(carros[3]);
+console.log(carros[4]);
