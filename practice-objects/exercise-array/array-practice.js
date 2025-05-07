f6623d2ae3077d4dd2034e38ac168d347f28090b
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-05-07 10:02:07 +0000

    practice array

--- practice-objects/exercise-array/array-practice.js
+++ practice-objects/exercise-array/array-practice.js
@@ -1,15 +1,37 @@
 let tarefas = ['lavar louças', 'Estudar javascript', 'Fazer exercicios', 'Limpar o quarto', 'Comprar pão'];
 
-tarefas.forEach(function(tarefa){
+tarefas.forEach(function(tarefa){ //mostra todas as tarefas na tela
     console.log(tarefa);
 });
 console.log('=========================================')
 
-tarefas.push('Estudar Inglês');
+tarefas.push('Estudar Inglês', 'Nadar', 'Jogar'); // adiciona mais uma tarefa ao array
 console.log('Tarefa adicionada');
 
 console.log('=========================================')
 
-tarefas.forEach(function(tarefa){
+tarefas.forEach(function(tarefa){ // mostra novamente todas as tarefas com a ultima tarefa adicionada
     console.log(tarefa);
 });
+
+console.log('=========================================')
+tarefas.pop() 
+console.log('Tarefa removida');
+console.log('=========================================')
+
+tarefas.forEach(function(tarefa){ // mostra novamente todas as tarefas com a ultima tarefa removida
+    console.log(tarefa);
+});
+
+console.log('=========================================') 
+const fiveLetters = tarefas.filter(tarefa => tarefa.length >= 5 ); // mostra apenas tarefas que contem cinco ou mais letras
+console.log(fiveLetters);
+
+console.log('=========================================') 
+const nomesMaiusculos = tarefas.map(function(tarefa){ // define todas as tarefas com letras maiúsculas
+    return tarefa.toUpperCase();
+});
+
+console.log(nomesMaiusculos);
+console.log('=========================================') 
+
