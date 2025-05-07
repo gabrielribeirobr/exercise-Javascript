eab0058b967858f4795974d27391cd2ea72de064
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-05-07 09:40:02 +0000

    practice array

--- practice-objects/exercise-array/array-practice.js
+++ practice-objects/exercise-array/array-practice.js
@@ -1,5 +1,15 @@
 let tarefas = ['lavar louças', 'Estudar javascript', 'Fazer exercicios', 'Limpar o quarto', 'Comprar pão'];
 
-tarefas.forEach(function(tarefa)){
-    console.log(tarefas);
-}
+tarefas.forEach(function(tarefa){
+    console.log(tarefa);
+});
+console.log('=========================================')
+
+tarefas.push('Estudar Inglês');
+console.log('Tarefa adicionada');
+
+console.log('=========================================')
+
+tarefas.forEach(function(tarefa){
+    console.log(tarefa);
+});
