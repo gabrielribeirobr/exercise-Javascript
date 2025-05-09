270da31f44a668f4b6170852e3c9b2c7851747b0
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-05-09 11:42:31 +0000

    simulator shopping cart

--- practice-objects/exercise-array/array-practice2.js
+++ practice-objects/exercise-array/array-practice2.js
@@ -3,21 +3,26 @@ const prompt = require('prompt-sync')();
 
 let shoppingCart = [];
 
-let ask = prompt('Deseja adicionar itens ao carrinho de compras? (sim/não): ');
+let ask = prompt('Deseja adicionar itens ao carrinho de compras? (sim/não): ').toLowerCase();
 
-const addItems = function(){
-    for(let i=0; i<=shoppingCart.length; i++){
-        shoppingCart[i] = prompt('Digite o nome do item à ser colocado no carrinho de compras: ');
-        console.log(ask ? addItems() : console.log('OK!', 'Este é o carrinho de compras atual: ', shoppingCart));
-    }
+function addItem(){
+    let item = prompt('Digite o nome do item a ser colocado no carrinho de compras: ');
+    shoppingCart.push(item);
+    ask = prompt('Deseja adicionar mais itens? (sim/não): ').toLowerCase();
+}
+while(ask == 'sim'){
+    addItem();
 }
 
-if(ask == 'sim'){
-    addItems();
-} else {
-    console.log('OK!');
-    console.log('Este é o carrinho de compras atual: ', shoppingCart);
+if(ask == 'nao' && shoppingCart.length == 0){
+     console.log('Carrinho vazio!')
+} else{
+    console.log('\nItens no carrinho:');
+    console.log(shoppingCart);
 }
+
+shoppingCart.length > 0 ? ask = prompt('Deseja remover algum item do carrinho? ') : console.log('Adeus');
+
 
 
 
